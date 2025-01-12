e61a6176170ebecba3ec0b1c799d6bd8ffb2d571
Ihar Baranau <baranovigorjsdev@gmail.com>
2025-01-12 19:14:26 +0100

    solution-8

---
+++ task-8-kyu/converter.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/557b5e0bddf29d861400005d/train/javascript
+
+function converter(mpg) {
+  return (mpg * 1.609344) / 4.54609188;
+}
+
+// const converter = mpg => mpg * 1.609344 / 4.54609188;
